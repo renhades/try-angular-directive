b42c9414c3473cbf2a9341212c85354243a9764e
ryan.liu <moomoo9513@gmail.com>
2015-10-10 10:43:23 +0800

    learn multi-directives call same method from outside controller

--- scripts/directives.loader-in-ctrl.js
+++ scripts/directives.loader-in-ctrl.js
@@ -1,9 +1,15 @@
 var myModule = angular.module('MyModule', []);
 
-myModule.controller('MyCtrl', ['$scope', function ($scope) {
-  $scope.loadData = function () {
-    console.log('數據加載中...');
+myModule.controller('MyCtrl1', ['$scope', function ($scope) {
+  $scope.loadData1 = function () {
+    console.log('1號數據加載中...');
   };
+}]);
+
+myModule.controller('MyCtrl2', ['$scope', function ($scope) {
+  $scope.loadData2 = function () {
+    console.log('2號數據加載中...');
+  }
 }]);
 
 myModule.directive('loader', function () {
@@ -11,7 +17,7 @@ myModule.directive('loader', function () {
     restrict: 'AE',
     link: function (scope, element, attr) {
       element.bind("mouseenter", function () {
-        scope.loadData();
+        scope.$apply(attr.howtoload); //注意: 呼叫屬性的方法時，要小寫
       });
     }
   }
